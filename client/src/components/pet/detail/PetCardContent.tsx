83099474e713612791717bd810c8300b92c8257c
Maidana <franco_maidana07@hotmail.com>
2024-10-28 21:18:58 -0300

    contenido basico de petcard

--- client/src/components/pet/detail/PetCardContent.tsx
+++ client/src/components/pet/detail/PetCardContent.tsx
@@ -1,8 +1,37 @@
 import { FC } from "react"
 
-const PetCardContent: FC<any> = () => {
+type Props = {
+  posted: string,
+  genre: string,
+  birthDate: string,
+  notes: string
+}
+
+const PetCardContent: FC<Props> = (props) => {
+  const { posted, genre, birthDate, notes } = props
+
   return (
-    <div>PetCardContent</div>
+    <div className="mt-6 mb-8 space-y-2 tracking-wide">
+      <p className="text-dark-gray text-body">
+        <span className="font-semibold mr-2">Publicado:</span>
+        {posted}
+      </p>
+
+      <p className="text-dark-gray text-body">
+        <span className="font-semibold mr-2">Género:</span>
+        {genre}
+      </p>
+
+      <p className="text-dark-gray text-body">
+        <span className="font-semibold mr-2">Edad:</span>
+        {birthDate}
+      </p>
+
+      <p className="text-dark-gray text-body">
+        <span className="font-semibold mr-2">Notas:</span>
+        {notes}
+      </p>
+    </div>
   )
 }
 
--- client/src/components/pet/detail/PetDetailCard.tsx
+++ client/src/components/pet/detail/PetDetailCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import PetCardContent from "./PetCardContent"
 import PetCardFooter from "./PetCardFooter"
 import PetCardHeader from "./PetCardHeader"
 
@@ -9,11 +10,17 @@ const PetDetailCard = () => {
     <div className="bg-white rounded-lg shadow-lg p-6 max-w-md my-8 mx-auto">
       <PetCardHeader
         type="Perro"
-        name="Reina"
+        name="Cocker"
         photoUrl="https://images.unsplash.com/photo-1560807707-8cc77767d783"
         distance="0.8"
       />
 
+      <PetCardContent
+        posted="15/10/24"
+        genre="Hembra"
+        birthDate="5 años"
+        notes="Encontré a esta perrita corriendo asustada por la Avenida Córdoba. Se nota que tiene dueño, tiene puesto un collar con su nombre."
+      />
 
       <PetCardFooter
         poster="Franco Maidana"
