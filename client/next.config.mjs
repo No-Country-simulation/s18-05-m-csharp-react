a548889ef949727d11bb1ef189488ce67784eaff
Maidana <franco_maidana07@hotmail.com>
2024-10-30 19:01:16 -0300

    cambiando a modo test para deploy

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_PATH: process.env.NEXT_PUBLIC_API_PATH,
-    MODE: "dev",
+    MODE: "test", // "dev" / "prod" / "test"
     EXPIRATION_TIME: String(60 * 60 * 24 * 7),
   },
   images: {
